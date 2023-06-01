043f6321722c19147e05ef0a979f15c2a04ca888
Gyan Academy <sngyan.acs@gmail.com>
2023-06-02 00:43:02 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+function gotowhatsapp() {
+
+let query = document.getElementById("newsletter1").value;
+
+let url=
+
+"https://wa.me/918088263376?text=" +
+
+"Hi Gyan Academy , i have query about... :- "
+
++ query ;
+
+window.open(url, "_blank").focus();
+
+}
+
+// document.querySelector(".cet").click(function(){
+
+//      window.location = "www.http://cetonline.karnataka.gov.in/kea/"; 
+
+//     });
