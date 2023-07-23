373994ae367c7d726baa6c268f59c4765925d8cc
Ajay Tainwala <sngyan.acs@gmail.com>
2023-07-23 22:58:07 +0530

    updated

--- form.js
+++ form.js
@@ -1,101 +1,105 @@
 //jshint esversion:6
 
-function gowhatsapp() {
-    let firstname = document.getElementById("firstName").value;
-    let lastname = document.getElementById("lastname").value;
-    let gender = document.getElementById("gender").value;
-    let dob = document.getElementById("dob").value;
-    let motherTongue = document.getElementById("m-tongue").value;
-    let pname = document.getElementById("pname").value;
-    let fatherOccupation = document.getElementById("occupation").value;
-    let motherName = document.getElementById("m-name").value;
-    let motherOccupation = document.getElementById("m-occupation").value;
-    let phone = document.getElementById("phone").value;
-    let parentsPhone = document.getElementById("pphone").value;
-    let email = document.getElementById("email").value;
-    let pphone = document.getElementById("pphone").value;
-    let houseBuildingno = document.getElementById("House-Building-no").value;
-    let Streetlocality = document.getElementById("Street-locality").value;
-    let district = document.getElementById("district").value;
-    let state = document.getElementById("state").value;
-    let pinCode = document.getElementById("pin-code").value;
+async function gowhatsapp() {
+    var firstname = document.getElementById("firstName").value;
+    var lastname = document.getElementById("lastname").value;
+    var gender = document.getElementById("gender").value;
+    var dob = document.getElementById("dob").value;
+    var motherTongue = document.getElementById("m-tongue").value;
+    var pname = document.getElementById("pname").value;
+    var fatherOccupation = document.getElementById("occupation").value;
+    var motherName = document.getElementById("m-name").value;
+    var motherOccupation = document.getElementById("m-occupation").value;
+    var phone = document.getElementById("phone").value;
+    var parentsPhone = document.getElementById("pphone").value;
+    var email = document.getElementById("email").value;
+    var pphone = document.getElementById("pphone").value;
+    var houseBuildingno = document.getElementById("House-Building-no").value;
+    var StreetLocality = document.getElementById("Street-locality").value;
+    var district = document.getElementById("district").value;
+    var state = document.getElementById("state").value;
+    var pinCode = document.getElementById("pin-code").value;
     var std = document.getElementById("std").value;
-    let scname = document.getElementById("scname").value;
+    var scname = document.getElementById("scname").value;
 
-    let stream = document.getElementById("stream").value;
-    let mode = document.getElementById("mode").value;
-    let board = document.getElementById("board").value;
-    let ref = document.getElementById("ref").value;
+    var stream = document.getElementById("stream").value;
+    var mode = document.getElementById("mode").value;
+    var board = document.getElementById("board").value;
+    var ref = document.getElementById("ref").valuvar
 
+    var markedCheckbox = document.getElementsByName('data[sub]');
+    var subjects = " ";
+    for (var i = 0; i < markedCheckbox.length; i++) {
+        if (markedCheckbox[i].checked) {
+            subjects += markedCheckbox[i].value + " ; ";
 
-    var markedCheckbox = document.querySelectorAll('#subjects>input[type="checkbox"]:checked');
-    for (var checkbox of markedCheckbox) {
-        var subjects = document.body.append(checkbox.value + ', ');
+        }
     }
-    
-
-    let url = "https://wa.me/918088263376?text="+
-        "Name of the student : " + "*" +
-        firstname + " " + lastname + "*" +
-        "%0a" +
-        "%0a" +
-        "Students Contact Number: " + 
-        phone +
-        "%0a" +
-        "%0a" +
-        "Gender : "
-        + gender +
-        "%0a" +
-        "%0a" +
-        "Date of Birth: "
-        + dob +
-        "%0a" +
-        "%0a" +
-        "Name of Father : "
-        + pname +
-        "%0a" +
-        "%0a" +
-        "Name of Mother : " + motherName +
-        "%0a" +
-        "%0a" +
-        "Parents Contact Number : "
-        + pphone +
-        "%0a" +
-        "%0a" +
-        "Residential Address : "
-        + houseBuildingno + "," + Streetlocality + ", " + district + ", " + state + ", " + pinCode +
-        "%0a" +
-        "%0a" +
-        "Class Studying : "
-        + std +
-        "%0a" +
-        "%0a" +
-        "School/College Studying : " + "*"
-        + scname + "*" +
-        "%0a" +
-        "%0a" +
-        "Subjects Needed : "
-        + subjects +
-        "%0a" +
-        "%0a" +
-        "Stream : "
-        + stream +
-        "%0a" +
-        "%0a" +
-        "Mode of Teaching : " + "*" +
-        mode + "*" +
-        "%0a" +
-        "%0a" +
-        "Board : "
-        + board;
+    return new Promise(resolve => {
 
 
-
+        var url = "https://wa.me/918088263376?text=" +
+            "Name of the student : " + "*" +
+            firstname + " " + lastname + "*" +
+            "%0a" +
+            "%0a" +
+            "Students Contact Number: " +
+            phone +
+            "%0a" +
+            "%0a" +
+            "Gender : "
+            + gender +
+            "%0a" +
+            "%0a" +
+            "Date of Birth: "
+            + dob +
+            "%0a" +
+            "%0a" +
+            "Name of Father : "
+            + pname +
+            "%0a" +
+            "%0a" +
+            "Name of Mother : " + motherName +
+            "%0a" +
+            "%0a" +
+            "Parents Contact Number : "
+            + pphone +
+            "%0a" +
+            "%0a" +
+            "Residential Address : "
+            + houseBuildingno + "," + StreetLocality + ", " + district + ", " + state + ", " + pinCode +
+            "%0a" +
+            "%0a" +
+            "Class Studying : "
+            + std +
+            "%0a" +
+            "%0a" +
+            "School/College Studying : " + "*"
+            + scname + "*" +
+            "%0a" +
+            "%0a" +
+            "Subjects Needed : "
+            + subjects +
+            "%0a" +
+            "%0a" +
+            "Stream : "
+            + stream +
+            "%0a" +
+            "%0a" +
+            "Mode of Teaching : " + "*" +
+            mode + "*" +
+            "%0a" +
+            "%0a" +
+            "Board : "
+            + board;
 
 
 
-    window.open(url, "_blank").focus();
 
+
+
+        window.open(url, "_blank").focus();
+    })
 }
 
 var currentTab = 0; // Current tab is set to be the first tab (0)
@@ -112,7 +116,9 @@ function showTab(n) {
         document.getElementById("prevBtn").style.display = "inline";
     }
     if (n == (x.length - 1)) {
-        document.getElementById("nextBtn").innerHTML=`<input type="submit" onsubmit="gowhatsapp() name="submit" id="submit"  >Submit`;
+        document.getElementById("nextBtn").innerHTML = 'Submit';
+        document.getElementById("nextBtn").setAttribute('type', 'submit');
+        document.getElementById("nextBtn").setAttribute('onsubmit', 'submitAllFunction()');
     } else {
         document.getElementById("nextBtn").innerHTML = "Next";
     }
@@ -152,7 +158,7 @@ function validateForm() {
         // If a field is empty...
         if (y[i].value == "") {
             // add an "invalid" class to the field:
-            y[i].className+=" invalid";
+            y[i].className += " invalid";
             // and set the current valid status to false
             valid = false;
         }
@@ -171,17 +177,46 @@ function fixStepIndicator(n) {
         x[i].classList = x[i].className.replace(" active", "");
     }
     //... and adds the "active" class on the current step:
-    x[n].className+=" active";
+    x[n].className += " active";
 }
 //  sending form to google sheets
-function sendtoGsheets() {
-    const scriptURL = 'https://script.google.com/macros/s/AKfycbx4rPzR2Sm4UHGWfguNA2j27rdD92exv6S9tFKBCsCH6bLr-oWnPXcFBPmIT18iNCC6fw/exec'
-    const form = document.forms['Student-Database']
+async function sendtoGsheets() {
+    return new Promise(resolve => {
 
-    form.addEventListener('submit', e => {
-        e.preventDefault()
-        fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-            .then(response => alert('Successfully Registered!', response))
-            .catch(error => alert('Error!', error.message))
+
+        const scriptURL = 'https://script.google.com/macros/s/AKfycbx4rPzR2Sm4UHGWfguNA2j27rdD92exv6S9tFKBCsCH6bLr-oWnPXcFBPmIT18iNCC6fw/exec'
+        const form = document.getElementById('regForm');
+
+        form.addEventListener('submit', e => {
+            e.preventDefault()
+            fetch(scriptURL, { 
+                method: 'POST',
+                 body: new FormData(form) ,
+                }).then(response => response.json()
+                )
+                .then((html)=>
+                {
+                    window.location.href ="";
+                });
+        });
+    });
+}
+
+async function sendEmail() {
+    return new Promise(resolve => {
+        Email.send({
+            SecureToken: "63d38ca0-7d6c-43c2-939f-3b2f8c70b59b",
+            To: 'sngyan.acs@gmail.com',
+            From: "ajaytainwala@gmail.com",
+            Subject: "New Student has Registered" + firstname + " " + lastname,
+            Body: url
+        }).then(
+            message => alert(message)
+        );
     })
+}
+async function submitAllFunction() {
+    await gowhatsapp();
+    await sendEmail();
+    await sendtoGsheets();
 }
