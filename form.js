f025055e2cee04e7aef0d98f53d09260f2ccc7b6
Ajay Tainwala <ajaytainwala@gmail.com>
2023-10-14 20:57:09 +0530

    Version Update

--- form.js
+++ form.js
@@ -1,13 +1,16 @@
-//jshint esversion:6
-function gotowhatsapp() {
-    let query = document.getElementById("newsletter1").value;
-    let url =
-        "https://wa.me/918088263376?text=" +
-        "Hi Gyan Academy , I have query about... :- "
-        + query;
-    window.open(url, "_blank").focus();
-}
-function gowhatsapp() {
+
+
+var markedCheckbox = document.getElementsByName('sub');
+    var subjects = " ";
+    for (var i = 0; i < markedCheckbox.length; i++) {
+        if (markedCheckbox[i].checked) {
+            subjects += markedCheckbox[i].value + " ; ";
+
+        }
+    }
+
+
+           function gowhatsapp() {
     let firstname = document.getElementById("firstName").value;
     let lastname = document.getElementById("lastname").value;
     let gender = document.getElementById("gender").value;
@@ -32,7 +35,7 @@ function gowhatsapp() {
     var stream = document.getElementById("stream").value;
     var mode = document.getElementById("mode").value;
     var board = document.getElementById("board").value;
-    var ref = document.getElementById("ref").valuvar
+    var ref = document.getElementById("ref").value;
 
     var markedCheckbox = document.getElementsByName('sub');
     var subjects = " ";
@@ -42,10 +45,9 @@ function gowhatsapp() {
 
         }
     }
-    return new Promise(resolve => {
 
     let url = "https://wa.me/918088263376?text="+
-        "Name of the student : " + "*" +
+    "Name of the student : " + "*" +
         firstname + " " + lastname + "*" +
         "%0a" +
         "%0a" +
@@ -70,6 +72,10 @@ function gowhatsapp() {
         "%0a" +
         "Parents Contact Number : "
         + pphone +
+        "%0a" +
+        "%0a" +
+        "Email-ID : "
+        + email +
         "%0a" +
         "%0a" +
         "Residential Address : "
@@ -97,15 +103,12 @@ function gowhatsapp() {
         "%0a" +
         "%0a" +
         "Board : "
-        + board;
-
-
-
+        + board+"%0a"+"%0a"+
+        "Reference :"+ ref;
 
 
 
         window.open(url, "_blank").focus();
-    })
 }
 
 var currentTab = 0; // Current tab is set to be the first tab (0)
@@ -124,7 +127,8 @@ function showTab(n) {
     if (n == (x.length - 1)) {
         document.getElementById("nextBtn").innerHTML = 'Submit';
         document.getElementById("nextBtn").setAttribute('type', 'submit');
-        document.getElementById("nextBtn").setAttribute('onsubmit', 'submitAllFunction()');
+        document.getElementById("nextBtn").setAttribute('onsubmit', 'sendtoGsheets()');
+        // document.getElementsByClassName("hidden").style.display = "block";
     } else {
         document.getElementById("nextBtn").innerHTML = "Next";
     }
@@ -143,10 +147,11 @@ function nextPrev(n) {
     // Increase or decrease the current tab by 1:
     currentTab = currentTab + n;
     // if you have reached the end of the form...
-    if (currentTab >= x.length) {
+    if (currentTab == x.length) {
         // ... the form gets submitted:
+        // document.getElementsByClassName("hidden").style.display = "block";
         document.getElementById("regForm").submit();
-        return false;
+        return true;
     }
 
     // Otherwise, display the correct tab:
@@ -186,42 +191,111 @@ function fixStepIndicator(n) {
     x[n].className += " active";
 }
 //  sending form to google sheets
-async function sendtoGsheets() {
-    return new Promise(resolve => {
+ function sendtoGsheets() {
 
+    var markedCheckbox = document.getElementsByName('sub');
+    var subjects = " ";
+    for (var i = 0; i < markedCheckbox.length; i++) {
+        if (markedCheckbox[i].checked) {
+            subjects += markedCheckbox[i].value + " ; ";
 
-        const scriptURL = 'https://script.google.com/macros/s/AKfycbyNg38Y-ko7UBTLq4gXvGpp9mBH9SUBxZVRARwuXtnVRC41cSveybvvHuouKSBkfIEHTg/exec'
-        const form = document.getElementById('regForm');
+        }
+    }
+        const scriptURL = 'https://script.google.com/macros/s/AKfycbyzhcyJc_DYPwoOe1Qr2VGmVj7mmvf7sor5IH_E8vYPVmSBtS3fRQdjVFjsbSfdqPBdxQ/exec'
+        const form = document.forms['submit-to-google-sheets']
 
         form.addEventListener('submit', e => {
             e.preventDefault()
             fetch(scriptURL, { 
                 method: 'POST',
                  body: new FormData(form) ,
-                }).then(response => response.json())
-                .then((html)=>
-                {
-                    window.location.href ="";
-                });
+                }).then(response => console.log('Registered Successfully!'))
+                .catch(error => console.error('Error!',error.message))
         });
-    });
+        sendEmail();
 }
+ function sendEmail() {
+    let firstname = document.getElementById("firstName").value;
+    let lastname = document.getElementById("lastname").value;
+    let gender = document.getElementById("gender").value;
+    let dob = document.getElementById("dob").value;
+    let motherTongue = document.getElementById("m-tongue").value;
+    let pname = document.getElementById("pname").value;
+    let fatherOccupation = document.getElementById("occupation").value;
+    let motherName = document.getElementById("m-name").value;
+    let motherOccupation = document.getElementById("m-occupation").value;
+    let phone = document.getElementById("phone").value;
+    let parentsPhone = document.getElementById("pphone").value;
+    let email = document.getElementById("email").value;
+    let pphone = document.getElementById("pphone").value;
+    let houseBuildingno = document.getElementById("House-Building-no").value;
+    let Streetlocality = document.getElementById("Street-locality").value;
+    let district = document.getElementById("district").value;
+    let state = document.getElementById("state").value;
+    let pinCode = document.getElementById("pin-code").value;
+    var std = document.getElementById("std").value;
+    var scname = document.getElementById("scname").value;
 
-async function sendEmail() {
-    return new Promise(resolve => {
+    var stream = document.getElementById("stream").value;
+    var mode = document.getElementById("mode").value;
+    var board = document.getElementById("board").value;
+    var ref = document.getElementById("ref").value;
+
+    var markedCheckbox = document.getElementsByName('sub');
+    var subjects = " ";
+    for (var i = 0; i < markedCheckbox.length; i++) {
+        if (markedCheckbox[i].checked) {
+            subjects += markedCheckbox[i].value + " ; ";
+
+        }
+    }
+    let sendtoEmail = 
+       "<strong>"+ "Name of the student : " +  "<strong>"+
+        firstname + " " + lastname + "  <br> " +
+     
+        "Students Contact Number: " +    phone +"  <br> " +
+       
+        "Gender : " + gender +"  <br> " +
+     
+        "Date of Birth: " + dob +"  <br> " +
+       
+        "Name of Father : " + pname +"  <br> " +
+       
+        "Name of Mother : " + motherName +
+        "  <br> " +
+        "Parents Contact Number : " + pphone +"  <br> " +
+        
+        "Email-ID : "   + email +"  <br> " +
+      
+        "Residential Address : " + houseBuildingno + "," + Streetlocality + ", " + district + ", " + state + ", " + pinCode +"  <br> " +
+        
+        "Class Studying : "  + std +"  <br> " +
+       
+        "School/College Studying : " + "*" + scname + "*" +"  <br> " +
+       
+        "Subjects Needed : "+ subjects +"  <br> " +
+        
+        "Stream : " + stream +"  <br> " +
+       
+        "Mode of Teaching : " + "*" + mode + "*" +"  <br> " +
+      
+        "Board : " + board+"  <br> " +
+
+        "Reference :"+ ref;
+
+
+
+
         Email.send({
-            SecureToken: "63d38ca0-7d6c-43c2-939f-3b2f8c70b59b",
+            SecureToken: "f5d1ba85-c160-427d-b0bb-e905d19db696 ",
             To: 'sngyan.acs@gmail.com',
             From: "ajaytainwala@gmail.com",
-            Subject: "New Student has Registered" + firstname + " " + lastname,
-            Body: url
+            Subject: "New Student has Registered  " + firstname + " " + lastname,
+            Body: sendtoEmail,
         }).then(
-            message => alert(message)
+            message => alert('Registered Successfully')
+        ).catch(
+            error => console.error('Error',error.message)
         );
-    })
 }
-async function submitAllFunction() {
-    await gowhatsapp();
-    await sendEmail();
-    await sendtoGsheets();
-}
+
